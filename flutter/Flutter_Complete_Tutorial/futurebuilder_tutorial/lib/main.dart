7abeec5980de29852a79fb5484a98eb37660e894
Samarpan Dasgupta <samarpan2dasgupta@gmail.com>
2023-03-31 08:57:39 +0530

    Future Builder Tutorial done

---
+++ flutter/Flutter_Complete_Tutorial/futurebuilder_tutorial/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+import 'main_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'API Integration',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MainScreen(),
+    );
+  }
+}
